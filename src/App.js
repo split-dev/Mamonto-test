2e2a31996ae33be0d8fd8231a0c40bf287cfd7ad
split-dev <asamusenko@splitdev.pro>
2021-05-12 14:56:24 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Route} from 'react-router-dom';
 import Login from './pages/Login';
 import React, {useState} from 'react';
+import {useSelector} from 'react-redux';
 
 import "./assets/scss/main.scss"
 import CreateCollection from './pages/Create-collections';
@@ -8,15 +9,14 @@ import CreateCollection from './pages/Create-collections';
 function App() {
 
   const [StatusAuth, setStatusAuth] = useState(localStorage.getItem('authorize') === 'true' ? true : false);
+  const { isAuth } = useSelector(({ user }) => user);
 
   const toggleAuth = () => {
-    setStatusAuth(!StatusAuth);
-    
-		console.log(StatusAuth);
+    setStatusAuth(localStorage.getItem('authorize') === 'true' ? true : false);
   }
 
   const logOut = () => {
-    toggleAuth();
+    setStatusAuth(false);
     localStorage.removeItem('token');
     localStorage.removeItem('authorize');
   }
@@ -25,7 +25,7 @@ function App() {
   return (
     <div className="App">
     {
-        StatusAuth ? <Route path="/" component={() => (<CreateCollection logOut={logOut}/>)} exact/> : <Login toggleAuth={toggleAuth}/>
+       isAuth || StatusAuth ? <Route path="/" component={() => (<CreateCollection logOut={logOut}/>)} exact/> : <Login toggleAuth={toggleAuth}/>
     }
       
       
